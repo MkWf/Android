9aca450066715f04ca3afd9748c539a41580f3b0
MkWf <markdevw@gmail.com>
2022-11-08 10:57:41 -0500

    Base bottom navigation implementation
    1)Gradle dependencies
    2)Create the fragments and their layouts
    3)Create a navigation graph listing the fragments
    4)Create the menu for the bottom navigation items and add their icons
    5)Add a navhost fragment and bottom navigation view to the activity layout
    6)Setup the bottom navigation view by pointing it to the navhost fragment

---
+++ app/src/main/java/com/example/android/ui/NewsActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.android.R
+import com.example.android.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Init view binding
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Get the fragment from the activity layout
+        binding.bottomNavigationView.setupWithNavController(supportFragmentManager.findFragmentById(R.id.newsNavHostFragment)!!.findNavController())
+    }
+}
