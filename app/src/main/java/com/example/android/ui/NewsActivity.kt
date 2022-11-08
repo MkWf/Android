87f47e3843ded7288604010cb3f9272cd468baaf
MkWf <markdevw@gmail.com>
2022-11-08 11:19:47 -0500

    The selected navigation fragment will display its title on the appbar
    1)Get the titles from the fragments in the menu and update the toolbar with the name when the fragment is changed

--- app/src/main/java/com/example/android/ui/NewsActivity.kt
+++ app/src/main/java/com/example/android/ui/NewsActivity.kt
@@ -3,6 +3,8 @@ package com.example.android.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.android.R
 import com.example.android.databinding.ActivityNewsBinding
@@ -20,5 +22,9 @@ class NewsActivity : AppCompatActivity() {
 
         //Get the fragment from the activity layout
         binding.bottomNavigationView.setupWithNavController(supportFragmentManager.findFragmentById(R.id.newsNavHostFragment)!!.findNavController())
+
+        //Get the titles from the fragments in the menu and update the toolbar with the name when the fragment is changed
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.breakingNewsFragment, R.id.savedNewsFragment, R.id.searchNewsFragment))
+        setupActionBarWithNavController(supportFragmentManager.findFragmentById(R.id.newsNavHostFragment)!!.findNavController(), appBarConfiguration)
     }
 }
