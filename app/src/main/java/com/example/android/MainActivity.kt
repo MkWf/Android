96c24146259bfea17eb19051f8bd5fa25a4db600
MkWf <markdevw@gmail.com>
2022-11-13 17:42:20 -0500

    Setting up
    1)Added Room and other dependencies for using injection
    2)Added database classes
    3)Test injection in mainactivity

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.android.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint //required when we inject inside of any Android component: fragment, activity, service, etc...
 class MainActivity : AppCompatActivity() {
+
+    @Inject //will look in our AppModule and see if theres any functions that creates RunDAO. Then itll see that we need a database to create the RUNDao object and create that as well
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}") //to test that it worked
     }
 }
--- app/src/main/java/com/example/android/di/AppModule.kt
+++ app/src/main/java/com/example/android/di/AppModule.kt
@@ -1,15 +1,33 @@
 package com.example.android.di
 
 import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.example.android.db.RunningDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class) //our dependencies will be created and destroyed inside of our application during onCreate() and onDestroy()
 object AppModule {
 
-    
+    @Singleton //only want one instance. if we didn't use this annotation, every class that needed this dependency would create a new instance
+    @Provides  //provides a database
+    fun provideRunningDatabase(
+        @ApplicationContext app: Context  //tells Dagger to use our app context
+    ) = Room.databaseBuilder(
+        app,
+        RunningDatabase::class.java,
+        "running.db"
+    )
+
+    @Singleton
+    @Provides //dagger will know how to make our database so that we can get a dao
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
 }
 //@InstallIn(SingletonComponent::class)  //will be available throughout the lifecylcle of our application
 //@InstallIn(ActivityComponent:class) //will only be available during the lifecycle of our activity
